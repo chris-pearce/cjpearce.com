231cfa09cb4dfdec9c2c52b1ea1e634346a945a6
Chris Pearce <crite70@gmail.com>
2017-04-30 20:03:41 +1000

    CSS and HTML minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,23 @@
-module.exports = {
+// http://eng.datafox.com/nodejs/2014/09/28/nodejs-config-best-practices/
+
+const config = {
     plugins: {
         'postcss-import': {},
-        'autoprefixer': {}
+        'autoprefixer': {grid: false}
+    }
+};
+
+module.exports = function(){
+    switch(process.env.NODE_ENV){
+        case 'development':
+            return config;
+            break;
+        case 'production':
+            config.plugins['postcss-csso'] = {};
+            return config;
+            break;
+        default:
+            console.error('Unrecognized NODE_ENV: ' + process.env.NODE_ENV);
+            process.exit(1);
     }
 };
